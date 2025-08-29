3481a3b729710549f6f818a3bebe9c2dd2f4aa8d
Pute008 <felixwestby@outlook.com>
2025-08-29 11:16:15 +0200

    nå funker if else statmenten
    nå kan du skrive din favoritt jobb, og console vil skrive forskjellige svar når du skriver din job

--- oppgavesamling/javaScript/variabler-IfElse/script.js
+++ oppgavesamling/javaScript/variabler-IfElse/script.js
@@ -1,16 +1,54 @@
-let array = [
-    'brannmann',
-    'politimann',
-    'koding',
-    'sykepleier'
-]
+// let array = [
+//     'brannmann',
+//     'politimann',
+//     'koding',
+//     'sykepleier'
+// ]
 
-document.getElementById("form").addEventListener("submit", favorittHobby);
+// document.getElementById("form").addEventListener("submit", favorittHobby);
 
-function favorittHobby(event) {
+// function favorittHobby(event) {
+//     event.preventDefault();
+//     console.warn("Test")
+// }
+
+// // function favorittHobby(event) {
+// //     event.preventDefault();
+// //     let favorittHobby = document.getElementById("favorittHobby");
+// //     if (favorittHobby) = array {
+// //         console.log("test");
+// //     }
+// // }
+
+
+const array = [1, 2, 3];
+
+console.log(array.includes(2));
+// Expected output: true
+
+
+
+
+
+
+
+const favorittJob = ["brannmann", "politi", "programmerer"];
+
+
+
+document.getElementById("favorittJobb").addEventListener("click", function(event) {
     event.preventDefault();
-    let favorittHobby = document.getElementById("favorittHobby");
-    if (favorittHobby) = array {
-        console.log("test");
+    let dinJob = prompt("Hva er favorittjobben din? ");
+
+    if (favorittJob.includes(dinJob)) {
+        console.log("Jeg liker også å jobbe med " + dinJob);
+    } else {
+        console.log("Jeg liker noe annet");
     }
-}
+});
+
+console.log(favorittJob.includes("brannmann"));
+// Expected output: true
+
+console.log(favorittJob.includes("politi"));
+// Expected output: false
