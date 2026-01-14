e10a6b34912fd3ff9d099475b21db03d265008fa
Pute008 <felixwestby@outlook.com>
2026-01-14 12:35:59 +0100

    du kan logge deg inn og opprette en ny bruker, men stien sender deg ikke til home.html, skal begynne på typescript

--- oppgavesamling/innlogging/app.js
+++ oppgavesamling/innlogging/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 const Database = require("better-sqlite3");
+const db = new Database("brukere.db");
+const app = express()
+
+const port = 3000;
+
+app.use(express.json());
+
+app.use(express.static("public"))
 
 app.use
     (session({
@@ -12,13 +20,20 @@ app.use
     })
 );
 
-app.post("/leggTilPerson", async (req, res) => {
-    const { id, fornavn, etternavn, passord } = req.body;
+function kreverInnlogging(req, res, next) {
+    if (!req.session.bruker) {
+        return res.redirect("/index.html")
+    };
+    next();
+}
 
-    const saltRounds = 10
-    const hashPassord = await bcrypt.hash(passord, saltRounds)
-    const stmt = db.prepare("");
-    const info = stmt.run(id, fornavn, etternavn, hashPassord);
+app.post("/leggTilPerson", async (req, res) => {
+    const { fornavn, etternavn, passord } = req.body;
+
+    const saltRounds = 10;
+    const hashPassord = await bcrypt.hash(passord, saltRounds);
+    const stmt = db.prepare("INSERT INTO brukere (fornavn, etternavn, passord) VALUES (?, ?, ?)");
+    const info = stmt.run(fornavn, etternavn, hashPassord);
 
     res.json({ message: "Ny person lagt til", info });
 })
@@ -26,16 +41,28 @@ app.post("/leggTilPerson", async (req, res) => {
 app.post("/login", async (req, res) => {
     const { fornavn, passord } = req.body;
 
-    const bruker = db.prepare("").get(fornavn);
+    const bruker = db.prepare("SELECT * FROM brukere WHERE fornavn = ?").get(fornavn);
     if (!bruker) {
         return res.status(401).json({ message: "Feil fornavn eller passord" });
     }
 
-    const passordErGyldig = await bcrypt.compare(bruker, bruker.passord);
+    const passordErGyldig = await bcrypt.compare(passord, bruker.passord);
     if (!passordErGyldig) {
         return res.status(401).json({ message: "Feil fornavn eller passord" });
     }
 
     req.session.bruker = { id: bruker.id, fornavn: bruker.fornavn };
-    res.json({ message: "Innlogging vellykket", redirect: "/dashboard" })
+    res.json({ message: "Innlogging vellykket", redirect: "/homeSide" })
 })
+
+app.get("/homeSide", kreverInnlogging, (req, res) => {
+    res.sendFile(__dirname + "../hidden/home.html");
+});
+
+app.use('/hidden', kreverInnlogging, express.static(path.join(__dirname, 'secure')));
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
+
+// app.use('/hidden', express.static(path.join(__dirname, 'hidden')));
