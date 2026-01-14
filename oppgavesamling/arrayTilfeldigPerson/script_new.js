8589763ef54c2d078105af9e3270bd29e2b71d1e
Pute008 <felixwestby@outlook.com>
2026-01-14 08:48:32 +0100

    innstalerte pakker til prosjektet
    node js pakker

--- oppgavesamling/arrayTilfeldigPerson/script_new.js
+++ oppgavesamling/arrayTilfeldigPerson/script_new.js
@@ -1,9 +1,21 @@
-let navn = ["Felix", "Mads", "Jakov"];
+// let navn = ["Felix", "Mads", "Jakov"];
 
+// let vinner = []
+
+// let antallVinnere = 2
+
+// for (let index = 0; index < antallVinnere; index++) {
+//     let n = Math.floor(Math.random() * navn.length)
+//     vinner.push(navn[n])
+//     navn.splice(n, 1)
+// }
+
+// console.table(vinner)
+
+// repetisjon
+let navn = ["felix", "alex", "tim"]
 let vinner = []
-
-let antallVinnere = 2
-
+let antallVinnere = 1
 for (let index = 0; index < antallVinnere; index++) {
     let n = Math.floor(Math.random() * navn.length)
     vinner.push(navn[n])
