f031360bbf9f2d063670a1addf2732185a05cbf1
Pute008 <felixwestby@outlook.com>
2026-03-09 11:28:10 +0100

    tjenester ud må ha i fjelltur oppgaven
    legger til requirements for hva du må ha for å kjøre programmet

--- oppgavesamling/fjellturOppgave/app.js
+++ oppgavesamling/fjellturOppgave/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+
 const Database = require("better-sqlite3");
 const db = new Database("fjelltur.db");
 
 const port = 3000;
+
